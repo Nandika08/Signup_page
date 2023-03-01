2f5b86a7b4641ea98d0bf1629741f0ea6215edd8
Nandika08 <126694406+Nandika08@users.noreply.github.com>
2023-03-01 20:55:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Signuppagetwo from './components/Signuppagetwo.js';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Signuppagetwo/>
+      
+    </div>
+  );
+}
+
+export default App;
