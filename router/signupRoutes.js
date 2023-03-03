24cc73d773467b5b57248fc154ef37a895f51640
Nandika08 <126694406+Nandika08@users.noreply.github.com>
2023-03-03 11:44:31 +0530

    Delete router directory

--- router/signupRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { userSignup } from '../Controller/signupController.js'
-
-const signupRouter = express.Router()
-
-signupRouter.post('/signup', userSignup)
-
-export default signupRouter
